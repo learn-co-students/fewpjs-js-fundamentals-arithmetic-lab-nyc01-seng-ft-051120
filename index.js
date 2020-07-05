0535f924a8a3b38efac973a36e2297d2c28a2160
emotionalproof <chaserabenn@gmail.com>
2020-07-05 01:14:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const num3 = 10
+const mod = num3 % 6
+
+const max = Math.max(1,2,4,20)
